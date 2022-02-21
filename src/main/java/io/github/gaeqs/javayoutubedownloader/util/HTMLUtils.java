1fa74409ce62e4e80f5f3019341a3c7b3d696906
Gael Rial Costas <g.rial.2018@alumnos.urjc.es>
2022-02-21 01:44:13 +0100

    JYD 1.2.3. Working with the last YouTube version. (21/02/2022)

--- src/main/java/io/github/gaeqs/javayoutubedownloader/util/HTMLUtils.java
+++ src/main/java/io/github/gaeqs/javayoutubedownloader/util/HTMLUtils.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 
 public class HTMLUtils {
 
-    public static String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36";
+    public static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0";
 
     /**
      * Reads all the text from an {@link URL}.
